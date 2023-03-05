f2ef1d600774c45b27d968cf07be8953ab269212
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-03-05 22:08:46 +0100

    Update mining/rigel/myrigel.sh

---
+++ mining/rigel/myrigel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script contains different command lines to install an run the differents version of the Rigel Miner
+# https://github.com/rigelminer/rigel
+# Feel free to use/adapt the differents lines
+
+
+#The following line is executed and the script exit
+
+
+exit 0
+
+#The following lines are not executed, there are only here to store and facilitate reuse of the script
+
+#Getting the last realease
