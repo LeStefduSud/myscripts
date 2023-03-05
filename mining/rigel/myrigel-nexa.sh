095842ad4585fd2c56ee5136dd2abcede1e9f0bf
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-03-05 22:10:47 +0100

    Update mining/rigel/myrigel-nexa.sh

--- mining/rigel/myrigel-nexa.sh
+++ mining/rigel/myrigel-nexa.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 # This script contains different command lines to install an run the differents version of the Rigel Miner
 # https://github.com/rigelminer/rigel
-# Feel free to use/adapt the differents lines
+#Use it as an example for my farm, composed in order of the graphic card AMD RX 5600XT, NIVDIA RTX 3060TI,NIVDIA RTX 3070TI,NIVDIA 3080 FE
 
 
 #The following line is executed and the script exit
+
+
+
+
+#Only the following command will be really executed
+./bzminer 
+pause
+exit 0
 
 
 exit 0
