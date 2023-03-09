924fc43b4f85e805eb46c12f3b076145d75b7ff3
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-03-09 21:10:35 +0100

    Update mining/bzminer/mybzminer-kaspa.sh

--- mining/bzminer/mybzminer-kaspa.sh
+++ mining/bzminer/mybzminer-kaspa.sh
@@ -7,7 +7,7 @@
 #The following line is executed and the script exit
 
 #Only the following command will be really executed
-sudo ./rigel -a nexapow -o stratum+ssl://fr.vipor.net:5184 -u nexa:nqtsq5g5wa882tktx2qj94qts96dkwcanw0dua9twh9ht6sk -w rig01 --cclock 210,255,255 --lock-cclock 1740,1680,1470 --lock-mclock 5001 --pl 170,200,270
+sudo ./bzminer -a kaspa -w kaspa:qq5xqy7u940dw9d9vnpey4pm9vuvkhl2x3e2kf225auwleh2u02vxffh5rmmp.rig01 -p stratum+ssl://de.kaspa.herominers.com:1206 --nc 1 --a2 zil --w2 zil10ej64utfe2ueczp284xw28yp73nn97pa9j9cg6 --p2 zmp+ssl://eu1-zil.shardpool.io:5555 --nvidia 1 --hide_disabled_devices --oc_lock_core_clock 1675 --oc_lock_memory_clock 810 --oc_core_clock_offset 250
 pause
 exit 0
 
