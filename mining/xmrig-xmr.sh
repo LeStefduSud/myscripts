d5b4e6b3e8de2be344a43f1fcdb9feb5e057cdbd
LeStefDuSud <lestefdusud@gmail.com>
2023-05-14 14:08:00 +0200

    Test

---
+++ mining/xmrig-xmr.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#Stef mining monero
+
+/opt/xmrig/xmrig --coin=XMR --threads=4 -o xmr.2miners.com:12222 -u 48DWUzoTANZAMakwHVKqJq6JT3Ah2SfFC5i5nVrkFVREVaKcPet8hP9MPFapEveUCr3mLZCmnChUG7THLu7maDENLvsyaZZ.hpprodesk --tls -p x
