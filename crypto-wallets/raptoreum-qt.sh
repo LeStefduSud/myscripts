d2fb1ac48912bbe876f50bf3aae8ca0b5f6bbf80
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-05-17 13:52:13 +0200

    Update raptoreum-qt.sh

--- crypto-wallets/raptoreum-qt.sh
+++ crypto-wallets/raptoreum-qt.sh
@@ -3,13 +3,13 @@
 #wget -O - https://raw.githubusercontent.com/LeStefduSud/myscripts/main/crypto-wallets/raptoreum-qt.sh | bash 
 
 # Download the latest version of the RaptoreumQT wallet
-wget https://github.com/Raptor3um/raptoreum/releases/latest/download/raptoreum-qt-linux-x64.tar.gz
+wget https://github.com/Raptor3um/raptoreum/releases/download/1.3.17.04/raptoreum-ubuntu22-1.3.17.04.tar.gz
 
 # Unpack the downloaded file to a directory of your choice
-tar -xvf raptoreum-qt-linux-x64.tar.gz
+tar -xvf raptoreum-ubuntu22-1.3.17.04.tar.gz
 
 # Copy the raptoreum-qt binary to /usr/local/bin
-sudo cp raptoreum-qt-linux-x64/raptoreum-qt /usr/local/bin
+sudo cp raptoreum-ubuntu22-1.3.17.04/raptoreum-qt /usr/local/bin
 
 # Create a desktop file to create a launcher for the wallet
 cat <<EOF | sudo tee /usr/share/applications/raptoreum-qt.desktop
@@ -23,4 +23,4 @@ Categories=Office;Finance;
 EOF
 
 # Delete the temporary files
-rm -rf raptoreum-qt-linux-x64.tar.gz raptoreum-qt-linux-x64
+rm -rf raptoreum-ubuntu22-1.3.17.04.tar.gz raptoreum-ubuntu22-1.3.17.04
