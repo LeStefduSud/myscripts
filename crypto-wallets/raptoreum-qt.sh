46f54d606e800b81e183a4b5fe5e66e8f1907bec
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-05-17 11:48:54 +0200

    Create raptoreum-qt.sh

---
+++ crypto-wallets/raptoreum-qt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Download the latest version of the RaptoreumQT wallet
+wget https://github.com/Raptor3um/raptoreum/releases/latest/download/raptoreum-qt-linux-x64.tar.gz
+
+# Unpack the downloaded file to a directory of your choice
+tar -xvf raptoreum-qt-linux-x64.tar.gz
+
+# Copy the raptoreum-qt binary to /usr/local/bin
+sudo cp raptoreum-qt-linux-x64/raptoreum-qt /usr/local/bin
+
+# Create a desktop file to create a launcher for the wallet
+cat <<EOF | sudo tee /usr/share/applications/raptoreum-qt.desktop
+[Desktop Entry]
+Name=RaptoreumQT
+Exec=raptoreum-qt
+Icon=/opt/raptoreum-qt/src/qt/res/icons/bitcoin.png
+Terminal=false
+Type=Application
+Categories=Office;Finance;
+EOF
+
+# Delete the temporary files
+rm -rf raptoreum-qt-linux-x64.tar.gz raptoreum-qt-linux-x64
