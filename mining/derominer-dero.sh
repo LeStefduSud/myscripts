77246dae34833cb91a99c437f92350d21f897f44
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-05-22 22:44:50 +0200

    Update derominer-dero.sh

--- mining/derominer-dero.sh
+++ mining/derominer-dero.sh
@@ -6,4 +6,7 @@ tar -xvf dero_linux_amd64.tar.gz
 mv dero_linux_amd64/* ./
 
 # starting
+./dero-miner-linux-amd64 --wallet-address=dero1qyph7msnjk0y5nrqwrydkaqsvwem6ypn9a89gkrpez3uesar0nt2kqguqpnqv
 
+
+./dero-miner-linux-amd64  --wallet-address deroi1qyph7msnjk0y5nrqwrydkaqsvwem6ypn9a89gkrpez3uesar0nt2kqdpvfz92xlrugqm0462yk7qwfhkpw --mining-threads 11 -r community-pools.mysrv.cloud:10300
