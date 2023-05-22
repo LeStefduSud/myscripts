4e70111109818ce689fed0235e7014e8b8f707c9
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-05-22 22:26:32 +0200

    Create derominer-dero.sh

---
+++ mining/derominer-dero.sh
@@ -0,0 +1,9 @@
+#Setup for mining Dero on CPU
+mkdir -p /opt/dero
+cd /opt/dero/
+wget https://github.com/deroproject/derohe/releases/download/Release117/dero_linux_amd64.tar.gz
+tar -xvf dero_linux_amd64.tar.gz
+mv dero_linux_amd64/* ./
+
+# starting
+
