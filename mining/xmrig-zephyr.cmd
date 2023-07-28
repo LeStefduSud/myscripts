d5c9cf92e26743c71d3662a86f44b4c1d8c18d2e
LeStefDuSud@w10-bidouille <lestefdusud@gmail.com>
2023-07-28 14:05:25 +0200

    Commit

--- mining/xmrig-zephyr.cmd
+++ mining/xmrig-zephyr.cmd
@@ -3,5 +3,5 @@ echo Mmining with XMRig fro Zephyr
 cd c:\miners\xmrig
 
 xmrig.exe -a rx/0 -o zeph.eu.ekapool.com:15666 ^
-  --threads=6 --donate-level 0 ^
+  --threads=4 --donate-level 0 ^
   -u ZEPHYR2rqxug1FyHuFT1YoVa8JSQMhSSS7SyFThLuwUxBxQCHJxGXYcgdZ1z1ZJqeN98Q9wLhp2DxcoheR35yQ6CZwmLfdPtvyS4d -k -p w10-bidouille
