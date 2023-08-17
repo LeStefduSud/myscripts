977c2c355800ed0b54a7fd220ccb1102ae699293
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-08-17 10:39:43 +0200

    Update decred-node-mining.sh

--- mining/decred-node-mining.sh
+++ mining/decred-node-mining.sh
@@ -1,16 +1,19 @@
 --Mounting and mining on decred using the new algo a partir du block : 794,368
 https://dcrdata.decred.org/
 
+--downloading an installing
 wget https://github.com/decred/decred-release/releases/download/v1.8.0/dcrinstall-linux-amd64-v1.8.0
 chmod +x dcrinstall-linux-amd64-v1.8.0
 ./dcrinstall-linux-amd64-v1.8.0
 
-suivre les instructions
-
+--suivre les instructions pour monter le node, pensez a sauver la seed lors de la création du wallet et attendre la synchro
 screen ./dcrd
+
+--démarrer le client du wallet, et demander une nouvelle addresse et la sauver
 screen ./dcrwallet
 ./dcrctl --wallet getnewaddress
 
+--Stopper le démon, pour le redemarrer plus tard avec l'adresse générée
 stop dcrd (CTRL + C dans screen)
 
 ./dcrd --miningaddr=DsUEAdz1xVtaU6GjozQuiEVhahjd9miGyvj
