1f3de56579e19a0b4c46e638d21a43c60916c3af
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-02 11:19:02 +0200

    Update decred-node-mining.sh

--- mining/decred-node-mining.sh
+++ mining/decred-node-mining.sh
@@ -3,21 +3,15 @@ https://dcrdata.decred.org/
 
 --downloading an installing giving us the dright
 sudo chown -Rv <user> /opt
-
 cd /opt
-sudo wget https://github.com/decred/decred-release/releases/download/v1.8.0/dcrinstall-linux-amd64-v1.8.0
+wget https://github.com/decred/decred-release/releases/download/v1.8.0/dcrinstall-linux-amd64-v1.8.0
 sudo chmod +x dcrinstall-linux-amd64-v1.8.0
-sudo ./dcrinstall-linux-amd64-v1.8.0
+./dcrinstall-linux-amd64-v1.8.0
 
 --suivre les instructions pour monter le node, pensez a sauver la seed lors de la création du wallet et attendre la synchro
 --redemarrage du node en précisant l'adresse de minage (ici sur mon adresse trustwallet)
-./dcrd --miningaddr=Dsau2BTh18vmRW9N6mC5P9GXe1jS32WaDCn
+/opt/decred/dcrd --miningaddr=Dsau2BTh18vmRW9N6mC5P9GXe1jS32WaDCn
 
 --on lance la commande d'activation du minage sur 8 thread par exemple
-./dcrctl setgenerate true 8
-./dcrctl getmininginfo
-
-./dcrctl --wallet getbalance
-./dcrctl --wallet getbalance DsZWtL6UmZLXk66E8GTdafJka2LwiY5kuNf
-
-./dcrctl --wallet sendtoaddress <address> <amount>
+/opt/decred/dcrctl setgenerate true 8
+/opt/decred/dcrctl getmininginfo.
