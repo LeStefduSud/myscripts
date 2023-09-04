e2866d19527f147f255e6e9264dbd0c0a1fde7a4
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-04 14:47:59 +0200

    Update xmrig-ramdomx-varse.sh

--- mining/xmrig-ramdomx-varse.sh
+++ mining/xmrig-ramdomx-varse.sh
@@ -2,6 +2,13 @@
 #Stef mining a GhostRider coin VarseShhain https://varsechain.com/#
 #On my Core Wallet VDB7tbv9qe6P8XTcY9hEfoWv2wZGYH55FA
 
+#on Tazmining
+sudo /opt/xmrig/xmrig --algo ghostrider --url stratum+tcp://pool.tazmining.ch:5301 --donate-level 0 \
+	--threads=4 --user VDB7tbv9qe6P8XTcY9hEfoWv2wZGYH55FA.ubuntu
+
+
+
+exit
 /opt/xmrig/xmrig -a ghostrider \
   -url stratum-eu.rplant.xyz:17093 --donate-level 0  --threads=4\
   --tls --user VDB7tbv9qe6P8XTcY9hEfoWv2wZGYH55FA.test01\
