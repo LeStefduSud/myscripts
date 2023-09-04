a85d4d9546db047050f9e71a141cb6208c8c159f
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-04 14:13:11 +0200

    Create xmrig-ramdomx-varse.sh

---
+++ mining/xmrig-ramdomx-varse.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Stef mining a GhostRider coin VarseShhain https://varsechain.com/#
+#On my Core Wallet
+
+/opt/xmrig/xmrig -a rx/0 \
+  -o de.monero.herominers.com:1111 --donate-level 0  --threads=4\
+  -u 48DWUzoTANZAMakwHVKqJq6JT3Ah2SfFC5i5nVrkFVREVaKcPet8hP9MPFapEveUCr3mLZCmnChUG7THLu7maDENLvsyaZZ \ 
+  -k -p ubuntu
