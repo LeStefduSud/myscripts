caeef57d5b31bf2b4fb7b372c4ea4dd0df57b8af
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-07 16:42:40 +0200

    Update xmrig-zephyr.cmd

--- mining/xmrig-zephyr.cmd
+++ mining/xmrig-zephyr.cmd
@@ -1,5 +1,10 @@
 echo Mmining with XMRig fro Zephyr
 
+--Under Linux
+/opt/xmrig/xmrig -a rx/0 \
+  -o zeph.eu.ekapool.com:15666 --threads=22 --donate-level 0 \
+  -u ZEPHYR2rqxug1FyHuFT1YoVa8JSQMhSSS7SyFThLuwUxBxQCHJxGXYcgdZ1z1ZJqeN98Q9wLhp2DxcoheR35yQ6CZwmLfdPtvyS4d -k -p rig01
+
 cd c:\miners\xmrig
 
 xmrig.exe -a rx/0 -o zeph.eu.ekapool.com:15666 ^
