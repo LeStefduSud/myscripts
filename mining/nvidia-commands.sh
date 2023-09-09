9f5e2f4d8772c0ab0aa51301af8171084cfb6088
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-09 13:53:50 +0200

    Update mining/nvidia-commands.sh

--- mining/nvidia-commands.sh
+++ mining/nvidia-commands.sh
@@ -3,6 +3,10 @@
 
 # Using nvidia-settings command line
 
-
+# getting list of GPUS found
+$ nvidia-smi -L
+GPU 0: NVIDIA GeForce RTX 3060 Ti (UUID: GPU-1d8b99ea-9f1a-2e3d-8253-1a82b196d3a5)
+GPU 1: NVIDIA GeForce RTX 3070 Ti (UUID: GPU-7db2b791-e594-b90d-5d7b-bc0990543dde)
+GPU 2: NVIDIA GeForce RTX 3080 (UUID: GPU-c54d145d-f1d4-5d68-dccb-a11548c6f79a)
 
 
