914b81c9a2e05b41433d8d2b101e67109add0356
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-09 13:45:55 +0200

    Update mining/nvidia-commands.sh

--- mining/nvidia-commands.sh
+++ mining/nvidia-commands.sh
@@ -1,8 +1,4 @@
 #Different command line for applying OC on command line
-mkdir -p /opt/dero
-cd /opt/dero/
-wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.1/astrominer-V1.9.1.R2_modern_amd64_linux.tar.gz
-tar -xvf astrominer-V1.9.1.R2_modern_amd64_linux.tar.gz
 
-# starting
-/opt/dero/astrominer/astrominer -w dero1qydg07mc8526nzv40vam8xczkxu0vm3la4lmkzre92pcuewk44zg6qgsszedj -r community-pools.mysrv.cloud:10300 -m 11
+
+
