36f9ef184344635f05fbc6a113e4e0749df1997d
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-09-09 13:54:47 +0200

    Update mining/nvidia-commands.sh

--- mining/nvidia-commands.sh
+++ mining/nvidia-commands.sh
@@ -9,4 +9,17 @@ GPU 0: NVIDIA GeForce RTX 3060 Ti (UUID: GPU-1d8b99ea-9f1a-2e3d-8253-1a82b196d3a
 GPU 1: NVIDIA GeForce RTX 3070 Ti (UUID: GPU-7db2b791-e594-b90d-5d7b-bc0990543dde)
 GPU 2: NVIDIA GeForce RTX 3080 (UUID: GPU-c54d145d-f1d4-5d68-dccb-a11548c6f79a)
 
-
+# getting info on aspecific GPU
+$ nvidia-smi -i 2
+Sat Sep  9 13:54:13 2023       
++---------------------------------------------------------------------------------------+
+| NVIDIA-SMI 535.86.05              Driver Version: 535.86.05    CUDA Version: 12.2     |
+|-----------------------------------------+----------------------+----------------------+
+| GPU  Name                 Persistence-M | Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp   Perf          Pwr:Usage/Cap |         Memory-Usage | GPU-Util  Compute M. |
+|                                         |                      |               MIG M. |
+|=========================================+======================+======================|
+|   2  NVIDIA GeForce RTX 3080        On  | 00000000:28:00.0 Off |                  N/A |
+| 65%   71C    P2             219W / 220W |    427MiB / 10240MiB |    100%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
