3fb487a6290d21b9a0a5e845b976d9d8493f7ede
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-10-04 11:57:40 +0200

    Added new node build instruction

--- mining/decred-node-mining.sh
+++ mining/decred-node-mining.sh
@@ -1,16 +1,35 @@
 --Mounting and mining on decred using the new algo a partir du block : 794,368
 https://dcrdata.decred.org/
 
---downloading an installing giving us the dright
+--downloading an installing giving us the dright using old version
 sudo chown -Rv <user> /opt
 cd /opt
 wget https://github.com/decred/decred-release/releases/download/v1.8.0/dcrinstall-linux-amd64-v1.8.0
 sudo chmod +x dcrinstall-linux-amd64-v1.8.0
 ./dcrinstall-linux-amd64-v1.8.0
 
+--Building latest node version from GitHub https://github.com/decred/dcrd#binaries-windowslinuxmacos
+-- Go installation https://go.dev/doc/install
+cd /tmp
+wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
+
+--Téléchargement et build de la dernière version du node
+$ git clone https://github.com/decred/dcrd $HOME/src/dcrd
+$ git clone https://github.com/decred/dcrctl $HOME/src/dcrctl
+$ (cd $HOME/src/dcrd && go install . ./...)
+$ (cd $HOME/src/dcrctl && go install)
+$ dcrd -V  ou /root/
+
+
 --suivre les instructions pour monter le node, pensez a sauver la seed lors de la création du wallet et attendre la synchro
 --redemarrage du node en précisant l'adresse de minage (ici sur mon adresse trustwallet)
-/opt/decred/dcrd --miningaddr=Dsau2BTh18vmRW9N6mC5P9GXe1jS32WaDCn
+---/opt/decred/dcrd --miningaddr=Dsau2BTh18vmRW9N6mC5P9GXe1jS32WaDCn
+--Lancer le node avec les info
+/root/go/bin/dcrd --miningaddr=Dsau2BTh18vmRW9N6mC5P9GXe1jS32WaDCn --rpcuser="stef2001" --rpcpass="pwd" --notls
+
+
+
 
 --on lance la commande d'activation du minage sur 8 thread par exemple
 /opt/decred/dcrctl setgenerate true 8
