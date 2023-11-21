6a50bb8375a01fc5b4622a9891059defa0ce9dea
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-11-21 13:38:59 +0100

    Update karlsen-solo.sh

--- mining/karlsen-solo.sh
+++ mining/karlsen-solo.sh
@@ -1,7 +1,7 @@
 # Mining on olo 
 # https://discord.com/channels/1024124284513308673/1175607307112026172/1175618025014165544  lien discord 
 
-#Installing the Node on a Ubuntu
+#01-Installing the Node on a Ubuntu
 #Voila la liste des commandes en avance sous UBUNTU 22 : 
 #Pensez a désactiver TOUS les parefeu sur le pot 42121
 mkdir karlsen && cd karlsen && wget https://github.com/karlsen-network/karlsend/releases/download/v1.0.0/karlsend-v1.0.0-linux.zip && unzip karlsend-v1.0.0-linux.zip
@@ -9,23 +9,31 @@ chmod +x *
 screen ./karlsend --utxoindex
 Attendre la sycnhro complete
 
+
 ctrl a+d
+
+#02-Creating a Wallet
 ./karlsenwallet create
 ./karlsenwallet start-daemon
 ./karlsenwallet new-address
 
-Chope ton adresse. Voila la mienne.
+#Chope ton adresse. Voila la mienne.
 karlsen:qrv820ag2wv5208p0acxxl0j0tu4hh4sp7q9hdrk88edwp7nn2ekgljcmly4t 
 Sauvegarde le fichier keys.json dans le rep .karlsen de ton user
 
+#03-Installer et lancer le Miner GPU, sous Hive OS
 Lancer le miner GPU: 
-1 - cd /
-2 - mkdir karlsen-miner
-3 - cd karlsen-miner
+ 
+2 - mkdir /opt/karlsen-miner
+3 - cd /opt/karlsen-miner
 4 - wget https://www.calabianca.fr/miner.zip
 5 - unzip miner.zip
 6 - chmod +x karlsen-miner
-7 - screen ./karlsen-miner --mining-address karlsen:qrv820ag2wv5208p0acxxl0j0tu4hh4sp7q9hdrk88edwp7nn2ekgljcmly4t  -s 192.168.1.8
+
+#Setting OC and launching Miner
+
+nvtool --setmem 5001 --setcore 1480 --setcoreoffset 270     directement dans le shell de hiveos , j ai des 3060ti
+/opt/karlsen-miner/karlsen-miner --mining-address karlsen:qrv820ag2wv5208p0acxxl0j0tu4hh4sp7q9hdrk88edwp7nn2ekgljcmly4t  -s ubuntu-worker02.lan
 
 En cas d'erreur 
 enleve la commande screen pour pouvoir voir l'erreur.
