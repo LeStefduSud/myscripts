fa1d6893a5b2e9b562d7c14be6015189cf659a08
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-11-28 22:22:38 +0100

    Update srbminer-karlsen.sh

--- mining/srbminer-karlsen.sh
+++ mining/srbminer-karlsen.sh
@@ -8,3 +8,12 @@ sudo nvtool --setmem 5001 --setcore 1500 --setcoreoffset 250 --setpl 150
 
 # on Herominer de.karlsen.herominers.com:1195
 SRBMiner-MULTI.exe --disable-cpu --algorithm karlsenhash --pool de.karlsen.herominers.com:1195 --wallet karlsen:qpmrrhwj0ph2vezz0dscgrjtd05fywen7cluagm23m9eg2d2z8lkj3f0z56va --password rig01
+
+
+#FS on hiveos
+--disable-cpu
+--gpu-cclock0 1500
+--gpu-coffset0 250 
+--gpu-mclock0 5001
+--gpu-plimit0 150
+--gpu-fan0 60
