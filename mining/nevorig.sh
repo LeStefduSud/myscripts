8c4431a93ece4f232641be3c77a0ef07988930a7
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-11-29 21:22:08 +0100

    Update nevorig.sh

--- mining/nevorig.sh
+++ mining/nevorig.sh
@@ -15,8 +15,12 @@ DEST=/opt/nevorig/
 #starting Mining in Rplant
 sudo /opt/nevorig/xmrig -a rx/nevo -o randomx.rplant.xyz:17102 --donate-level 0  --threads=23  -u NS27Dr6wxBxZbjFLk7Dx6iSqiTPcaaqWWMT3coVTzUfXCWL1uiRubAoC2FqY22p4gjZYARRDJXVkt3CoiEgWk4Bv2cqNuTfrU   -k -p rig-worker01
 
-#starting Mining in https://minergalaxy.eu/
-sudo /opt/nevorig/xmrig -a rx/nevo -o  161.97.82.235:1311 --donate-level 0  --threads=24 -u NS27Dr6wxBxZbjFLk7Dx6iSqiTPcaaqWWMT3coVTzUfXCWL1uiRubAoC2FqY22p4gjZYARRDJXVkt3CoiEgWk4Bv2cqNuTfrU   -k -p rig-worker01
+#HiveOS FlightSheet
+Cpu Config :
+"cpu": { "huge-pages": true, "hw-aes": null, "priority": null, "memory-pool": false, "asm": true, "rx1": [0], "rx": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22] }
+User Config :
+"randomx": { "1gb-pages": true } "donate-level": 1
+
 
 
 
