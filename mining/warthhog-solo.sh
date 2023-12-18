0102cd25ade33d9b82b32aac77bfb25ae3687d06
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-12-18 21:54:54 +0100

    Update warthhog-solo.sh

--- mining/warthhog-solo.sh
+++ mining/warthhog-solo.sh
@@ -3,7 +3,21 @@
 #Building the node from source
 #Href https://github.com/warthog-network/warthog-guide
 
+cd /opt/Warthog/build/src/node/
 #Starting th node
+/opt/Warthog/build/src/node/wart-node -r 0.0.0.0:3000
+
+#waiting for the node to synch and create a wallet adress
+/opt/Warthog/build/src/wallet/wart-wallet -c
+#Save the wallet.json that contains private and public KEY !! Do not share
+
+#Dipslay the public adress, and this is my public adress of my node
+/opt/Warthog/build/src/wallet/wart-wallet -a
+
+#Building and starting the Jasmine MIner
+#https://github.com/CoinFuMasterShifu/janusminer
+
+#building an once building launch it with OC
 
 
 #Starting the miner
