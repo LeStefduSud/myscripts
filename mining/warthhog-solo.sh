d303c41ac64857cd775882d2a0895358658e4011
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2023-12-18 22:57:18 +0100

    Update warthhog-solo.sh

--- mining/warthhog-solo.sh
+++ mining/warthhog-solo.sh
@@ -23,6 +23,13 @@ sudo apt install ocl-icd-opencl-dev
 
 
 #building an once building launch it with OC
+# On LolMiner --coff 300,300,300 --cclk 2300,2600,2600 --mclk 6001,6001,6001 --pl 180,250,250 --fan 100,100,100
+sudo nvidia-smi -pm 1
+sudo nvidia-smi -lgc 1500
+sudo nvidia-smi -lmc 5001
+sudo nvidia-smi -pl 125
+
+/opt/janusminer/build/src/miner/wart-miner -a 4af9ccf834a941af6869e8bf3ef020a74aa381161fb7f3b1 --gpus 0,1
 
 
 #Starting the miner
