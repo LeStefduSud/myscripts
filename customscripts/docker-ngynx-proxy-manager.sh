de1468ecee10bc1669aa721f4dba5d96ba532ec8
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2024-06-14 12:53:50 +0200

    Create docker-ngynx-proxy-manager.sh

---
+++ customscripts/docker-ngynx-proxy-manager.sh
@@ -0,0 +1,26 @@
+#Creating a dir and editing the docker-compose.yml file
+mkdir -p .pi./ngynx-proxy-mananger
+cd ngynx-proxy-mananger/
+nano docker-compose.yml
+
+#Us the basic config infomartion
+Version: 3
+services: 
+  app:    
+  image: 'jc21/nginx-proxy-manager:latest'    
+  container-name: proxy-manager    
+  restart: unless-stopped    
+  ports:
+      - '80:80' 
+      – '81:81'      
+      - '443:443'    
+  volumes:
+    – ./data:/data
+    – ./passencryption:/etc/talencrypt
+volumes:  
+    data:  
+    letsencrypt:
+
+#Save the file
+#Starting the composed stack in background
+docker-compose up -d
