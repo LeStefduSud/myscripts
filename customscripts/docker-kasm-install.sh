fba9cd9d6be60d478bf10674b5cbb5f37f0782d9
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2024-06-30 23:08:27 +0200

    Update docker-kasm-install.sh

--- customscripts/docker-kasm-install.sh
+++ customscripts/docker-kasm-install.sh
@@ -14,4 +14,4 @@ sudo bash kasm_release/install.sh -L 9443
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.14.0.3a7abb.tar.gz
 tar -xf kasm_release_1.14.0.3a7abb.tar.gz
-sudo bash kasm_release/install.sh
+sudo bash kasm_release/install.sh -L 9443
