0ffe4550eda71389daae408bb5d41d5b6ea5f756
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2024-07-17 13:54:18 +0200

    Create OpnSense-Install.sh

---
+++ customscripts/OpnSense-Install.sh
@@ -0,0 +1,15 @@
+# This is my snippet to install OpnSense, an OpenSource firwall
+# See https://opnsense.org/ or https://opnsense.org/download/
+# See https://opnsense.org/users/get-started/ to get more information how to ins
+---
+
+- Download the ISO or the VGA file
+- Burnit to a USB stick (or other) using Rufus or othe
+- Configure your system to boot from USB.
+- Login using default creadentials
+- Default behaviour is to start the Live environment, to install log in with user installer and password opnsense
+
+
+
+
+
