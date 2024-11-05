8c4494ec109f68de1254a7a51544a38adebc8ae9
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2024-11-05 17:50:20 +0100

    Update docker-kasm-install.sh

--- customscripts/docker-kasm-install.sh
+++ customscripts/docker-kasm-install.sh
@@ -12,8 +12,8 @@ sudo bash kasm_release/install.sh -L 9443
 
 #installtion of docker Kasmweb
 cd /tmp
-curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.14.0.3a7abb.tar.gz
-tar -xf kasm_release_1.14.0.3a7abb.tar.gz
+curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.0.a1d5b7.tar.gz
+tar -xf kasm_release_1.16.0.a1d5b7.tar.gz
 sudo bash kasm_release/install.sh -L 9443
 
 # Using Proot-apps to install without privilege and persistance application
