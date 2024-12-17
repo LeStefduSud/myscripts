4189ac5183d3e0ff7453e5c993aa38809a9a4083
LeStefduSud <124881509+LeStefduSud@users.noreply.github.com>
2024-12-17 22:12:06 +0100

    Update docker-base.sh

--- customscripts/docker-base.sh
+++ customscripts/docker-base.sh
@@ -32,8 +32,13 @@ sudo docker run hello-world
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
-#Installing the latest version of GO in one single line
+#Installing the latest version of GO in one single line for amd64
 VERSION="1.23.4" && wget -q "https://golang.org/dl/go${VERSION}.linux-amd64.tar.gz" -O go.tar.gz && sudo tar -C /usr/local -xzf go.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile && source ~/.profile && go version
+
+#Installing for Arm64
+VERSION="1.23.4" && wget -q "https://golang.org/dl/go${VERSION}.linux-arm64.tar.gz" -O go.tar.gz && sudo tar -C /usr/local -xzf go.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile && source ~/.profile && go version
+
+
 
 #Installing some cool stuff to manage docker
 #https://github.com/jesseduffield/lazydocker
